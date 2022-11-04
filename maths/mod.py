5a25e6008cd4f23990bd1f4ef3a3f5e6af18cbb4
Veeresh Katageri <veeresh.katageri@embitel.de>
2022-11-04 11:01:36 +0100

    Add modulus operator

---
+++ maths/mod.py
@@ -0,0 +1,12 @@
+def mod(num1, num2):
+    """This method will be used perform modulus operation on num1 by num2
+
+        :param int num1: The first number
+        :param int num2: The second number
+
+        :returns: The answer
+
+        :rtype: int
+    """
+    answer = num1 % num2
+    return answer
